e8de930a6768ac352590a904f98f46c51e736412
nakyong82 <nakyong7770@gmail.com>
2024-09-29 00:10:20 +0900

    feat: metadata 적용하기

---
+++ src/app/aboutus/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const metadata = {
+  title: "about us",
+};
+
+const AboutUsPage = () => {
+  return <main>aboutusPage</main>;
+};
+
+export default AboutUsPage;
